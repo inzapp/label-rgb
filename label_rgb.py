6b6de84d776a3297d72ff54e46721c622f958eff
inzapp <inzapp@naver.com>
2021-08-03 20:29:05 +0900

    code refactoring

--- label_rgb.py
+++ label_rgb.py
@@ -7,7 +7,7 @@ import numpy as np
 
 g_max_num_circles = 2
 g_win_size = (768, 768)
-g_win_name = 'Label RGB v1.0 by Inzapp'
+g_win_name = 'Label RGB v2.0 by Inzapp'
 
 
 def get_next_circle_index():
@@ -18,30 +18,62 @@ def get_next_circle_index():
     return g_circle_index
 
 
+def is_cursor_in_image(x):
+    global g_win_size
+    return x < g_win_size[0]
+
+
+def is_cursor_in_circle(x, y):
+    if is_cursor_in_image(x):
+        return False
+    return True
+
+
+def show_cursor_color(cur_x, cur_y):
+    global g_win_size
+    radius = int(min(g_win_size[0], g_win_size[1]) * 0.125)
+    raw_height, raw_width = g_raw.shape[0], g_raw.shape[1]
+    bgr = g_raw[cur_y][cur_x]
+    x = cur_x + radius
+    y = cur_y - radius
+    if cur_x > raw_width - radius:
+        x = cur_x - radius
+    if cur_y < radius:
+        y = cur_y + radius
+    raw_copy = g_raw.copy()
+    raw_copy = cv2.circle(raw_copy, (x, y), radius, (int(bgr[0]), int(bgr[1]), int(bgr[2])), thickness=-1)
+    cv2.imshow(g_win_name, raw_copy)
+
+
+def set_circle_to_side_pan_and_save_label(cur_x, cur_y):
+    global g_raw
+    bgr = g_raw[cur_y][cur_x] / 255.0
+    rgb = [bgr[2], bgr[1], bgr[0]]
+    circle_index = get_next_circle_index()
+    g_raw = set_circle_to_side_pan(g_raw, rgb, circle_index)
+    save_label(g_label_path, rgb, circle_index)
+    cv2.imshow(g_win_name, g_raw)
+
+
 def mouse_callback(event, cur_x, cur_y, flag, _):
     global g_raw
 
     # no click mouse moving
     if event == 0 and flag == 0:
-        radius = 100
-        raw_height, raw_width = g_raw.shape[0], g_raw.shape[1]
-        bgr = g_raw[cur_y][cur_x]
-        x = cur_x + radius
-        y = cur_y - radius
-        if cur_x > raw_width - radius:
-            x = cur_x - radius
-        if cur_y < radius:
-            y = cur_y + radius
-        raw_copy = g_raw.copy()
-        raw_copy = cv2.circle(raw_copy, (x, y), radius, (int(bgr[0]), int(bgr[1]), int(bgr[2])), thickness=-1)
-        cv2.imshow(g_win_name, raw_copy)
+        if is_cursor_in_image(cur_x):
+            show_cursor_color(cur_x, cur_y)
+        else:
+            cv2.imshow(g_win_name, g_raw)
 
-    # end click
+    # end left click
     elif event == 4 and flag == 0:
-        bgr = g_raw[cur_y][cur_x] / 255.0
-        rgb = [bgr[2], bgr[1], bgr[0]]
-        g_raw = set_circle_to_side_pan(g_raw, rgb, get_next_circle_index())
-        cv2.imshow(g_win_name, g_raw)
+        if is_cursor_in_image(cur_x):
+            set_circle_to_side_pan_and_save_label(cur_x, cur_y)
+
+    # right click
+    elif event == 5 and flag == 0:
+        if is_cursor_in_circle(cur_x, cur_y):
+            pass
 
 
 def side_pan():
@@ -62,6 +94,43 @@ def side_pan():
     return pan
 
 
+def save_label(label_path, rgb, circle_index):
+    # confidence = rgb[0]
+    r, g, b = rgb[0], rgb[1], rgb[2]
+    label_str = f'{r:.6f} {g:.6f} {b:.6f}\n'
+    if not (os.path.exists(label_path) and os.path.isfile(label_path)):
+        with open(label_path, 'wt') as f:
+            f.writelines(label_str)
+        return
+
+    with open(label_path, 'rt') as f:
+        lines = f.readlines()
+
+    if circle_index < len(lines):
+        lines[circle_index] = label_str
+    else:
+        lines.append(label_str)
+
+    with open(label_path, 'wt') as f:
+        f.writelines(lines)
+
+
+def set_circle_to_side_pan(img, rgb, circle_index):
+    global g_label_path
+    r, g, b = rgb
+    r = int(r * 255)
+    g = int(g * 255)
+    b = int(b * 255)
+    if circle_index == 0:
+        circle_cy = int(g_win_size[1] * 0.2)
+        img = cv2.circle(img, (g_side_pan_circle_cx, circle_cy), g_side_pan_circle_radius, (b, g, r), thickness=-1)
+
+    if circle_index == 1:
+        circle_cy = int(g_win_size[1] * 0.8)
+        img = cv2.circle(img, (g_side_pan_circle_cx, circle_cy), g_side_pan_circle_radius, (b, g, r), thickness=-1)
+    return img
+
+
 def load_saved_rgbs_if_exist(label_path):
     rgbs = list()
     if os.path.exists(label_path) and os.path.isfile(label_path):
@@ -75,44 +144,20 @@ def load_saved_rgbs_if_exist(label_path):
         return None
 
 
-def save_label(label_path, rgb, circle_index):
-    rgb_str = f'{rgb[0]:.6f} {rgb[1]:.6f} {rgb[2]:.6f}\n'
-    if not (os.path.exists(label_path) and os.path.isfile(label_path)):
-        with open(label_path, 'wt') as f:
-            f.writelines(rgb_str)
-        return
-
-    with open(label_path, 'rt') as f:
-        lines = f.readlines()
-
-    if circle_index < len(lines):
-        lines[circle_index] = rgb_str
+def get_g_side_pan_circle_positions():
+    global g_max_num_circles, g_side_pan_circle_radius
+    cys = []
+    if g_max_num_circles == 1:
+        cys = [0.5]
+    elif g_max_num_circles == 2:
+        cys = [0.2, 0.8]
     else:
-        lines.append(rgb_str)
-
-    with open(label_path, 'wt') as f:
-        f.writelines(lines)
-
-
-def set_circle_to_side_pan(img, rgb, circle_index):
-    global g_label_path
-    r, g, b = rgb
-    r = int(r * 255)
-    g = int(g * 255)
-    b = int(b * 255)
-    radius = int(side_pan_width / 2)
-    pan_x = g_win_size[0] + int((g_win_size[0] / 5) / 2)
-
-    if circle_index == 0:
-        pan_y = int(g_win_size[1] * 0.2)
-        img = cv2.circle(img, (pan_x, pan_y), radius, (b, g, r), thickness=-1)
-        save_label(g_label_path, rgb, circle_index)
-
-    if circle_index == 1:
-        pan_y = int(g_win_size[1] * 0.8)
-        img = cv2.circle(img, (pan_x, pan_y), radius, (b, g, r), thickness=-1)
-        save_label(g_label_path, rgb, circle_index)
-    return img
+        print('not implemented : g_max_num_circles > 2')
+        exit(0)
+    cys = np.asarray(cys) * g_win_size[1]
+    cys = cys.astype('int32')
+    cys = list(cys)
+    return cys
 
 
 path = ''
@@ -124,11 +169,14 @@ png_file_paths = glob(f'{path}*.png')
 img_paths = jpg_file_paths + png_file_paths
 if len(img_paths) == 0:
     print('No image files in path. run label.py with path argument')
-    sys.exit(0)
+    exit(0)
 
-side_pan = side_pan()
-side_pan_height = side_pan.shape[0]
-side_pan_width = side_pan.shape[1]
+g_side_pan = side_pan()
+g_side_pan_width = g_side_pan.shape[1]
+g_side_pan_height = g_side_pan.shape[0]
+g_side_pan_circle_radius = int(g_side_pan_width / 2)
+g_side_pan_circle_cx = g_win_size[0] + int((g_win_size[0] / 5) / 2)
+g_side_pan_circle_positions = get_g_side_pan_circle_positions()
 
 index = 0
 while True:
@@ -137,7 +185,7 @@ while True:
     g_label_path = f'{file_path[:-4]}.txt'
     g_raw = cv2.imread(file_path, cv2.IMREAD_COLOR)
     g_raw = cv2.resize(g_raw, g_win_size)
-    g_raw = np.concatenate((g_raw, side_pan), axis=1)
+    g_raw = np.concatenate((g_raw, g_side_pan), axis=1)
     g_rgbs = load_saved_rgbs_if_exist(g_label_path)
     if g_rgbs is not None:
         for i in range(len(g_rgbs)):
